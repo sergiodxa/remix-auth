d766ae13bdfb70150ebdd2ca5c2660b617adcd72
Sergio Xalambrí <hello@sergiodxa.com>
2021-08-30 22:48:04 -0500

    Auth0Strategy support customizing the scope

---
+++ test/strategies/auth0.test.ts
@@ -0,0 +1,103 @@
+import { createCookieSessionStorage, Request } from "remix";
+import { Auth0Strategy } from "../../src";
+
+describe(Auth0Strategy, () => {
+  let callback = jest.fn();
+  let verify = jest.fn();
+  let sessionStorage = createCookieSessionStorage({
+    cookie: { secrets: ["s3cr3t"] },
+  });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test("should allow changing the scope", async () => {
+    let strategy = new Auth0Strategy(
+      {
+        domain: "test.fake.auth0.com",
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+        scope: "custom",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/auth0");
+
+    let response = await strategy.authenticate(
+      request,
+      sessionStorage,
+      { sessionKey: "user" },
+      callback
+    );
+
+    let location = response.headers.get("Location");
+
+    if (!location) throw new Error("No redirect header");
+
+    let redirectUrl = new URL(location);
+
+    expect(redirectUrl.searchParams.get("scope")).toBe("custom");
+  });
+
+  test("should have the scope `openid profile email` as default", async () => {
+    let strategy = new Auth0Strategy(
+      {
+        domain: "test.fake.auth0.com",
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/auth0");
+
+    let response = await strategy.authenticate(
+      request,
+      sessionStorage,
+      { sessionKey: "user" },
+      callback
+    );
+
+    let location = response.headers.get("Location");
+
+    if (!location) throw new Error("No redirect header");
+
+    let redirectUrl = new URL(location);
+
+    expect(redirectUrl.searchParams.get("scope")).toBe("openid profile email");
+  });
+
+  test("should correctly format the authorization URL", async () => {
+    let strategy = new Auth0Strategy(
+      {
+        domain: "test.fake.auth0.com",
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/auth0");
+
+    let response = await strategy.authenticate(
+      request,
+      sessionStorage,
+      { sessionKey: "user" },
+      callback
+    );
+
+    let location = response.headers.get("Location");
+
+    if (!location) throw new Error("No redirect header");
+
+    let redirectUrl = new URL(location);
+
+    expect(redirectUrl.hostname).toBe("test.fake.auth0.com");
+    expect(redirectUrl.pathname).toBe("/authorize");
+  });
+});
