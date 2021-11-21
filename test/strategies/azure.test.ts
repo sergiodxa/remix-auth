ed3bfb92099a8fb3fc1004491f4cc65eb9b21b24
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2021-11-21 21:22:00 +0200

    Add tests for AzureStrategy

---
+++ test/strategies/azure.test.ts
@@ -0,0 +1,132 @@
+import { createCookieSessionStorage } from "@remix-run/server-runtime";
+import { AzureStrategy } from "../../src";
+
+describe(AzureStrategy, () => {
+  let verify = jest.fn();
+  let sessionStorage = createCookieSessionStorage({
+    cookie: { secrets: ["s3cr3t"] },
+  });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test("should allow changing the scope", async () => {
+    let strategy = new AzureStrategy(
+      {
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+        scope: "custom",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/azure");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, {
+        sessionKey: "user",
+      });
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.searchParams.get("scope")).toBe("custom");
+    }
+  });
+
+  test("should have the scope `openid profile email` as default", async () => {
+    let strategy = new AzureStrategy(
+      {
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/azure");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, {
+        sessionKey: "user",
+      });
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.searchParams.get("scope")).toBe(
+        "openid profile email"
+      );
+    }
+  });
+
+  test("should correctly format the authorization URL", async () => {
+    let strategy = new AzureStrategy(
+      {
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/azure");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, {
+        sessionKey: "user",
+      });
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.hostname).toBe("login.microsoftonline.com");
+      expect(redirectUrl.pathname).toBe("/common/oauth2/v2.0/authorize");
+    }
+  });
+
+  test("should allow changing tenant", async () => {
+    let strategy = new AzureStrategy(
+      {
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+        tenant: "custom",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/azure");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, {
+        sessionKey: "user",
+      });
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.pathname).toBe("/custom/oauth2/v2.0/authorize");
+    }
+  });
+});
