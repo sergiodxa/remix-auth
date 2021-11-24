fb30595af6ab096c8410f7403aa4145ee33c3c72
Sergio Xalambrí <hello@sergiodxa.com>
2021-11-24 00:47:58 -0500

    Authorization (#67)
    * Install Remix Utils
    
    * Create Authorizer and Rule
    
    * Create function version and allow passing rules on each call
    
    * Update authorizer.ts
    
    * Remove createAuthorizer and add more tests
    
    * Allow throwing errors

---
+++ src/authorizer.ts
@@ -0,0 +1,101 @@
+import { LoaderFunction, redirect } from "@remix-run/server-runtime";
+import { forbidden, unauthorized } from "remix-utils";
+import { Authenticator } from "./authenticator";
+
+type LoaderArgs = Parameters<LoaderFunction>[0];
+
+type AuthorizeArgs<User, Data> = Omit<RuleContext<User, Data>, "user">;
+
+export interface RuleContext<User, Data = unknown> extends LoaderArgs {
+  /**
+   * The authenticated user returned by the Authenticator
+   */
+  user: User;
+  /**
+   * Extra data passed to the Authorizer from the loader or action
+   */
+  data?: Data;
+}
+
+/**
+ * A Rule is a function that receives the same arguments of a Loader or Action
+ * and the authenticated user (as configured in the Authenticator), and maybe
+ * an extra data value.
+ *
+ * Inside a Rule function you can do any validation to ensure a user to continue
+ * and return a promise resolving to a boolean value.
+ */
+export interface RuleFunction<User, Data = unknown> {
+  (context: RuleContext<User, Data>): Promise<boolean>;
+}
+
+export class Authorizer<User = unknown, Data = unknown> {
+  constructor(
+    private authenticator: Authenticator<User>,
+    private rules: RuleFunction<User, Data>[] = []
+  ) {}
+
+  async authorize(
+    args: AuthorizeArgs<User, Data>,
+    options?: {
+      failureRedirect?: never;
+      raise?: "error";
+      rules?: RuleFunction<User, Data>[];
+    }
+  ): Promise<User>;
+  async authorize(
+    args: AuthorizeArgs<User, Data>,
+    options?: {
+      failureRedirect?: never;
+      raise?: "response";
+      rules?: RuleFunction<User, Data>[];
+    }
+  ): Promise<User>;
+  async authorize(
+    args: AuthorizeArgs<User, Data>,
+    options: {
+      failureRedirect: string;
+      raise: "redirect";
+      rules?: RuleFunction<User, Data>[];
+    }
+  ): Promise<User>;
+  async authorize(
+    args: AuthorizeArgs<User, Data>,
+    {
+      failureRedirect,
+      raise = "response",
+      rules = [],
+    }: {
+      failureRedirect?: string;
+      raise?: "error" | "response" | "redirect";
+      rules?: RuleFunction<User, Data>[];
+    } = {}
+  ): Promise<User> {
+    let user = await this.authenticator.isAuthenticated(args.request);
+
+    if (!user) {
+      if (raise === "response") {
+        throw unauthorized({ message: "Not authenticated." });
+      }
+      if (raise === "redirect") {
+        // @ts-expect-error failureRedirect is a string if raise is redirect
+        throw redirect(failureRedirect);
+      }
+      throw new Error("Not authenticated.");
+    }
+
+    for (let rule of [...this.rules, ...rules]) {
+      if (await rule({ user, ...args })) continue;
+      // @ts-expect-error failureRedirect is a string if raise is redirect
+      if (raise === "redirect") throw redirect(failureRedirect);
+      if (raise === "response") {
+        if (!rule.name) throw forbidden({ message: "Forbidden" });
+        throw forbidden({ message: `Forbidden by policy ${rule.name}` });
+      }
+      if (!rule.name) throw new Error("Forbidden.");
+      throw new Error(`Forbidden by policy ${rule.name}`);
+    }
+
+    return user;
+  }
+}
---
+++ test/authorizers.test.ts
@@ -0,0 +1,136 @@
+import {
+  createCookieSessionStorage,
+  redirect,
+} from "@remix-run/server-runtime";
+import { forbidden, unauthorized } from "remix-utils";
+import { Authenticator, Authorizer } from "../src";
+
+describe(Authorizer, () => {
+  type User = {
+    id: number;
+    token: string;
+    email: string;
+    role: string;
+  };
+
+  let user: User = {
+    id: 1,
+    token: "token",
+    email: "test@example.com",
+    role: "admin",
+  };
+
+  let sessionStorage = createCookieSessionStorage({
+    cookie: { name: "session", secrets: ["s3cr3ts"], path: "/" },
+  });
+
+  let authenticator = new Authenticator<User>(sessionStorage);
+
+  test("it should return the user as result", async () => {
+    let session = await sessionStorage.getSession();
+    session.set(authenticator.sessionKey, user);
+
+    let request = new Request("/", {
+      headers: { Cookie: await sessionStorage.commitSession(session) },
+    });
+
+    let authorizer = new Authorizer<User, string>(authenticator, [
+      async function isAdmin({ user }) {
+        return user.role === "admin";
+      },
+    ]);
+
+    await expect(
+      authorizer.authorize({ request, params: { id: "1" }, context: {} })
+    ).resolves.toEqual(user);
+  });
+
+  test("if user is not logged in throw a Unauthorized response", async () => {
+    let request = new Request("/", {});
+
+    let authorizer = new Authorizer<User, string>(authenticator, [
+      async function isAdmin({ user }) {
+        return user.role === "admin";
+      },
+    ]);
+
+    await expect(
+      authorizer.authorize({ request, params: { id: "1" }, context: {} })
+    ).rejects.toEqual(unauthorized({ message: "Not authenticated." }));
+  });
+
+  test("if user is not logged in an failureRedirect is defined redirect", async () => {
+    let request = new Request("/", {});
+
+    let authorizer = new Authorizer<User, string>(authenticator, [
+      async function isAdmin({ user }) {
+        return user.role === "admin";
+      },
+    ]);
+
+    await expect(
+      authorizer.authorize(
+        { request, params: { id: "1" }, context: {} },
+        { raise: "redirect", failureRedirect: "/login" }
+      )
+    ).rejects.toEqual(redirect("/login"));
+  });
+
+  test("if user doesn't pass rule throw a Forbidden response", async () => {
+    let session = await sessionStorage.getSession();
+    session.set(authenticator.sessionKey, user);
+
+    let request = new Request("/", {
+      headers: { Cookie: await sessionStorage.commitSession(session) },
+    });
+
+    let authorizer = new Authorizer<User, string>(authenticator, [
+      async function isNotAdmin({ user }) {
+        return user.role !== "admin";
+      },
+    ]);
+
+    await expect(
+      authorizer.authorize({ request, params: { id: "1" }, context: {} })
+    ).rejects.toEqual(forbidden({ message: "Forbidden by policy isNotAdmin" }));
+  });
+
+  test("if user doesn't pass rule throw a Forbidden response without the policy name if it's an arrow function", async () => {
+    let session = await sessionStorage.getSession();
+    session.set(authenticator.sessionKey, user);
+
+    let request = new Request("/", {
+      headers: { Cookie: await sessionStorage.commitSession(session) },
+    });
+
+    let authorizer = new Authorizer<User, string>(authenticator, [
+      async ({ user }) => user.role !== "admin",
+    ]);
+
+    await expect(
+      authorizer.authorize({ request, params: { id: "1" }, context: {} })
+    ).rejects.toEqual(forbidden({ message: "Forbidden" }));
+  });
+
+  test("if user doesn't pass rule and failureRedirect is defined throw a redirect", async () => {
+    let session = await sessionStorage.getSession();
+    session.set(authenticator.sessionKey, user);
+
+    let request = new Request("/", {
+      headers: { Cookie: await sessionStorage.commitSession(session) },
+    });
+
+    let authorizer = new Authorizer<User, string>(authenticator, [
+      async function isNotAdmin({ user }) {
+        return user.role !== "admin";
+      },
+    ]);
+
+    await expect(
+      authorizer.authorize(
+        { request, params: { id: "1" }, context: {} },
+        { raise: "redirect", failureRedirect: "/login" }
+      )
+    ).rejects.toEqual(redirect("/login"));
+  });
+});
