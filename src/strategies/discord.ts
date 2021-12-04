f839739957b288c9a48024124cfa74540ad58ab8
Lennart Gastler <lennart@lgastler.de>
2021-12-04 10:08:34 +0100

    Update Discord Strategy to extend OAuth2Profile

--- src/strategies/discord.ts
+++ src/strategies/discord.ts
@@ -1,4 +1,8 @@
-import { OAuth2Strategy, OAuth2StrategyVerifyCallback } from "./oauth2";
+import {
+  OAuth2Profile,
+  OAuth2Strategy,
+  OAuth2StrategyVerifyCallback,
+} from "./oauth2";
 
 export interface DiscordStrategyOptions {
   clientID: string;
@@ -17,73 +21,78 @@ export interface DiscordStrategyOptions {
 /**
  * @
  */
-export interface DiscordProfile {
-  /**
-   * the user's id
-   */
+export interface DiscordProfile extends OAuth2Profile {
   id: string;
-  /**
-   * the user's username, not unique across the platform
-   */
-  username: string;
-  /**
-   * the user's 4-digit discord-tag
-   */
-  discriminator: string;
-  /**
-   * the user's avatar hash
-   * @see https://discord.com/developers/docs/reference#image-formatting
-   */
-  avatar?: string;
-  /**
-   * whether the user belongs to an OAuth2 application
-   */
-  bot?: boolean;
-  /**
-   * whether the user is an Official Discord System user (part of the urgent message system)
-   */
-  system?: boolean;
-  /**
-   * whether the user has two factor enabled on their account
-   */
-  mfa_enabled?: boolean;
-  /**
-   * the user's banner hash
-   * @see https://discord.com/developers/docs/reference#image-formatting
-   */
-  banner?: string;
-  /**
-   * the user's banner color encoded as an integer representation of hexadecimal color code
-   */
-  accent_color?: string;
-  /**
-   * the user's chosen language option
-   */
-  locale?: string;
-  /**
-   * 	whether the email on this account has been verified
-   */
-  verified?: boolean;
-  /**
-   * the user's email
-   */
-  email?: string;
-  /**
-   * the flags on a user's account
-   * @see https://discord.com/developers/docs/resources/user#user-object-user-flags
-   */
-  flags?: number;
-  /**
-   * the type of Nitro subscription on a user's account
-   * @see https://discord.com/developers/docs/resources/user#user-object-premium-types
-   */
-  premium_type?: number;
-  /**
-   * the public flags on a user's account
-   * @see https://discord.com/developers/docs/resources/user#user-object-user-flags
-   */
-  public_flags?: number;
-  provider: "discord";
+  displayName: string;
+  emails?: [{ value: string }];
+  photos?: [{ value: string }];
+  __json: {
+    /**
+     * the user's id
+     */
+    id: string;
+    /**
+     * the user's username, not unique across the platform
+     */
+    username: string;
+    /**
+     * the user's 4-digit discord-tag
+     */
+    discriminator: string;
+    /**
+     * the user's avatar hash
+     * @see https://discord.com/developers/docs/reference#image-formatting
+     */
+    avatar?: string;
+    /**
+     * whether the user belongs to an OAuth2 application
+     */
+    bot?: boolean;
+    /**
+     * whether the user is an Official Discord System user (part of the urgent message system)
+     */
+    system?: boolean;
+    /**
+     * whether the user has two factor enabled on their account
+     */
+    mfa_enabled?: boolean;
+    /**
+     * the user's banner hash
+     * @see https://discord.com/developers/docs/reference#image-formatting
+     */
+    banner?: string;
+    /**
+     * the user's banner color encoded as an integer representation of hexadecimal color code
+     */
+    accent_color?: string;
+    /**
+     * the user's chosen language option
+     */
+    locale?: string;
+    /**
+     * 	whether the email on this account has been verified
+     */
+    verified?: boolean;
+    /**
+     * the user's email
+     */
+    email?: string;
+    /**
+     * the flags on a user's account
+     * @see https://discord.com/developers/docs/resources/user#user-object-user-flags
+     */
+    flags?: number;
+    /**
+     * the type of Nitro subscription on a user's account
+     * @see https://discord.com/developers/docs/resources/user#user-object-premium-types
+     */
+    premium_type?: number;
+    /**
+     * the public flags on a user's account
+     * @see https://discord.com/developers/docs/resources/user#user-object-user-flags
+     */
+    public_flags?: number;
+  };
 }
 
 export interface DiscordExtraParams extends Record<string, string | number> {
@@ -145,7 +154,17 @@ export class DiscordStrategy<User> extends OAuth2Strategy<
         Authorization: `Bearer ${accessToken}`,
       },
     });
-    let profile: DiscordProfile = await response.json();
+    let raw: DiscordProfile["__json"] = await response.json();
+
+    let profile: DiscordProfile = {
+      provider: "discord",
+      id: raw.id,
+      displayName: raw.username,
+      emails: raw.email ? [{ value: raw.email }] : undefined,
+      photos: raw.avatar ? [{ value: raw.avatar }] : undefined,
+      __json: raw,
+    };
+
     return profile;
   }
 }
