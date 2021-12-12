7a2dd683e3b4474aa9262534ef33f382cee1ac2a
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-12-12 05:46:13 +0530

    fix the bug in the github callback of example app.

--- examples/github/app/routes/auth.github.callback.tsx
+++ examples/github/app/routes/auth.github.callback.tsx
@@ -1,8 +1,8 @@
 import { LoaderFunction } from "remix";
 import { authenticator } from "~/services/auth.server";
 
-export let loader: LoaderFunction = async ({ request }) => {
-  await authenticator.authenticate("github", request, {
+export let loader: LoaderFunction = ({ request }) => {
+  return authenticator.authenticate("github", request, {
     successRedirect: "/",
     failureRedirect: "/login",
   });
