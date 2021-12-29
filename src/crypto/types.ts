a598f781d6f7211afb80a6fad5bf60a66cf37f46
Sergio Xalambrí <hello@sergiodxa.com>
2021-12-28 20:25:21 -0500

    Remove strategies already published in other packages (#110)
    * Remove strategies already published in other packages
    
    * Remove tests

--- src/crypto/types.ts
+++
@@ -1,5 +0,0 @@
-export interface Crypto {
-  generateKey(secret: string): Promise<ArrayBuffer>;
-  encrypt(key: ArrayBuffer, value: string): Promise<string>;
-  decrypt(key: ArrayBuffer, value: string): Promise<string>;
-}
